30e356fa1429634c6644cc8ae7e0e09b4f5a419f
Hannah Ton <hannaht2006@gmail.com>
2022-09-01 10:58:43 -0400

    Updated BrowserUtil file

--- src/utilities/BrowserUtils.java
+++ src/utilities/BrowserUtils.java
@@ -3,5 +3,7 @@ package utilities;
 public class BrowserUtils {
     public static void main(String[] args) {
         System.out.println("created");
+
+        System.out.println("Updated");
     }
 }
